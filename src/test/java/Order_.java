6eeb2cb2f50d38276ac741375f1dcc043e551869
Jaime Barreto <yaimesil@gmail.com>
2020-01-21 18:16:49 +0000

    Test 2: Red

--- src/test/java/Order_.java
+++ src/test/java/Order_.java
@@ -7,4 +7,9 @@ public class Order_ {
     public void given_an_empty_string_return_empty_string() {
         assertThat(Order.order("")).isEqualTo("");
     }
+
+    @Test
+    public void given_a_string_with_text_and_numbers_order_them() {
+        assertThat(Order.order("is2 Thi1s T4est 3a")).isEqualTo("Thi1s is2 3a T4est");
+    }
 }
