6ba7c42349e8db078efac6707bd9d2f1ce35635a
Jaime Barreto <yaimesil@gmail.com>
2020-01-22 20:42:10 +0000

    Test 2: Green

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,5 +1,34 @@
-public class Order {
-    public static String order(String words) {
-        return "";
+import java.util.Arrays;
+
+import static java.lang.Character.isDigit;
+
+class Order {
+    static String order(String words) {
+        if (words.isEmpty()) return "";
+        String[] wordsSplit = words.split(" ");
+        String[] wordsSplitOrdered = new String[wordsSplit.length];
+
+        for (String s : wordsSplit) {
+            wordsSplitOrdered[getNumber(s) - 1] = s + " ";
+        }
+
+        return toProperString(wordsSplitOrdered);
+    }
+
+    private static String toProperString(String[] wordsSplitOrdered) {
+        String res = "";
+        for (String s :
+                wordsSplitOrdered) {
+            res+=s;
+        }
+        return res.substring(0, res.length()-1);
+    }
+
+    private static int getNumber(String args) {
+        char[] chars = args.toCharArray();
+        for (char c : chars) {
+            if (isDigit(c)) return Character.getNumericValue(c);
+        }
+        return 0;
     }
 }
--- src/test/java/Order_.java
+++ src/test/java/Order_.java
@@ -10,6 +10,6 @@ public class Order_ {
 
     @Test
     public void given_a_string_with_text_and_numbers_order_them() {
-        assertThat(Order.order("is2 Thi1s T4est 3a")).isEqualTo("Thi1s is2 3a T4est");
+        assertThat(Order.order("is2aa Thi1s T4est 3a")).isEqualTo("Thi1s is2aa 3a T4est");
     }
 }
